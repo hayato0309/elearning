84296f2e92dcc5b8e2ce99cb299ac5fd5bb9b7a7
hayato0309 <45285649+hayato0309@users.noreply.github.com>
2019-05-29 23:35:45 +0800

    make dashboard layout (#2)
    home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,42 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+   <div class="row">
+        <div class="col-sm-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <figure class="text-center"><img src="{{ asset('images/dog.png') }}" alt="" class="rounded-circle" height="100px"></figure>
+                    <h3 class="text-center">Hayato</h3>
+                    <div class="text-center"><a class="btn btn-primary" href="">Edit Profile</a></div>
+                    <hr>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="text-center"><a href="">2</a></div>
+                                <div class="text-center">followings</div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-center"><a href="">2</a></div>
+                                <div class="text-center">followers</div>
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                    <br>
+                    <div class="card">
+                        <div class="text-center"><a href="">2</a></div>
+                        <div class="text-center">words learnded</div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="col-sm-8">
+            <div class="card">
+                <h3 class="card-header">Activity</h3>
+                <div class="card-body">
+                    <div>This is activity log.</div>
+                </div>
+            </div>
+        </div>
+   </div>
 </div>
 @endsection
